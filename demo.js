d561e1ec90dd6421cbad709c3ad2800653e6c517
lingaraj2020 <lingarajhonasani2020@gmail.com>
2023-02-10 18:10:02 +0530

    task5 commit

--- demo.js
+++ demo.js
@@ -50,7 +50,21 @@
 // }
 
 //QUERY SELECTORS//
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
 
+var input=document.querySelector('input');
+input.value='Hello world';
+
+var submit=document.querySelector('input[type="submit"]');
+submit.value="SEND";
+
+var item=document.querySelector('.list-group-item');
+item.style.color='red';
+var lastItem=document.querySelector('.list-group-item:last-child');
+lastItem.style.color='blue';
+var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color='coral';
 
 
 
